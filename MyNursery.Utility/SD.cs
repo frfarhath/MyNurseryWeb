f1f9f157c55bb1a513e5fce2ecffe68b48dd8544
frfarhath <frfarhath21@gmail.com>
2025-07-17 20:39:28 +0530

    Include SD.cs changes

--- MyNursery.Utility/SD.cs
+++ MyNursery.Utility/SD.cs
@@ -3,7 +3,7 @@
     public static class SD
     {
         public const string Role_Admin = "NuAD";
-        public const string Role_OtherUser = "NuOU";
+        public const string Role_OtherUser = "NuOUS"; //CHANGED NuOU INTO NuOUS
         public const string Role_User = "NuUS";
 
         public const string Success_Msg = "success";  
@@ -27,5 +27,9 @@
         public const string Status_Draft = "Draft";
         public const string Status_Archived = "Archived";
 
+        //Users
+        //public const string Role_NUUS = "NUUS";
+        public const string Role_SuperAdmin = "NuSAD";
+        public const string Role_AdminCSAD = "CsAD";
     }
 }
