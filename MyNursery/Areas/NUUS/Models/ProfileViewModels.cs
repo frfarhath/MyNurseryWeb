3cbb8686d3d7a5e131c8640b498e42bc89879209
frfarhath <frfarhath21@gmail.com>
2025-07-28 16:44:40 +0530

    Profile fixed

--- MyNursery/Areas/NUUS/Models/ProfileViewModels.cs
+++ MyNursery/Areas/NUUS/Models/ProfileViewModels.cs
@@ -3,30 +3,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyNursery.Areas.NUUS.Models
 {
-    
-   public class ChangePasswordViewModel
-        {
-            [Required]
-            [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
-            public string CurrentPassword { get; set; } = null!;
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "New password")]
-            public string NewPassword { get; set; } = null!;
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string CurrentPassword { get; set; } = null!;
 
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; } = null!;
-        }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; } = null!;
 
-        public class ProfileViewModel
-        {
-            public ApplicationUser User { get; set; } = null!;
-            public ChangePasswordViewModel ChangePasswordModel { get; set; } = null!;
-        }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = null!;
     }
 
+    public class ProfileViewModel
+    {
+        public ApplicationUser User { get; set; } = null!;
+        public ChangePasswordViewModel ChangePasswordModel { get; set; } = null!;
+    }
+}
