496c14fdd5c86cba881776dcf1b156882edd4f59
frfarhath <frfarhath21@gmail.com>
2025-07-28 15:00:52 +0530

    fix: update SD.cs before rebase

--- MyNursery.Utility/SD.cs
+++ MyNursery.Utility/SD.cs
@@ -3,8 +3,8 @@
     public static class SD
     {
         public const string Role_Admin = "NuAD";
-        public const string Role_OtherUser = "NuOUS"; //CHANGED NuOU INTO NuOUS
-        public const string Role_User = "NuUS";
+        public const string Role_OtherUser = "NuUS"; //Admin Added
+        public const string Role_User = "NuOUS"; //Registered User
 
         public const string Success_Msg = "success";  
         public const string Info_Msg = "info";        
@@ -28,8 +28,12 @@
         public const string Status_Archived = "Archived";
 
         //Users
-        //public const string Role_NUUS = "NUUS";
         public const string Role_SuperAdmin = "NuSAD";
         public const string Role_AdminCSAD = "CsAD";
+
+        // User Types
+        public const string UserType_Predefined = "Predefined";
+        public const string UserType_Registered = "Registered";
+        public const string UserType_AdminAdded = "AdminAdded";
     }
 }
